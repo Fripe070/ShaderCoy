c073cf9f6a4a6edf0cd243390f5e5141919ab6e8
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2025-08-06 22:55:41 +0200

    Support installing as a progressive web app

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,32 @@
+import type { APIRoute } from "astro";
+import { getImage } from "astro:assets";
+import appIcon from "../assets/images/pwa_icon.png";
+
+export async function GET() {
+    const icons = await Promise.all(
+        [192, 512, 1024].map(async (size) => {
+            const image = await getImage({
+                src: appIcon,
+                width: size,
+                height: size,
+                format: "png",
+            });
+            return {
+                src: image.src,
+                type: `image/${image.options.format}`,
+                sizes: `${image.options.width}x${image.options.height}`,
+            };
+        }),
+    );
+    return new Response(
+        JSON.stringify({
+            short_name: "ShaderCoy",
+            name: "ShaderCoy Shader Editor",
+            description: "A GLSL shader editor",
+            icons,
+            display: "standalone",
+            background_color: "#21252b",
+        }),
+        { headers: { "Content-Type": "application/json" } },
+    );
+}
