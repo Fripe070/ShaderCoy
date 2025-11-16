3e75e2051d197e40ec4abf43db9c7b4f9d6e082e
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2025-11-16 21:11:26 +0100

    Prep for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,9 @@ const config = {
 		adapter: staticAdapter({
 			fallback: "404.html",
 		}),
+		paths: {
+			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+		},
 	},
 	compilerOptions: {
 		runes: true,
