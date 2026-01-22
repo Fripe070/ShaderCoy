f16f4e9e117c51bb055ac3d29122695e35c134eb
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2026-01-22 21:03:39 +0100

    I am a tad dense

--- src/routes/manifest.webmanifest/+server.ts
+++ src/routes/manifest.webmanifest/+server.ts
@@ -1,5 +1,7 @@
 import { webManifest } from "$lib/components/MetaTags.svelte";
 
+export const prerender = true;
+
 export async function GET() {
 	return new Response(JSON.stringify(webManifest), {
 		headers: { "Content-Type": "application/json" },
